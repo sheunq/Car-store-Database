a63934b687d41220fde92b310fde956eb7c76182
sheunq <45465445+sheunq@users.noreply.github.com>
2022-04-19 21:27:24 +0100

    Update Subsqueries.sql

--- Subsqueries.sql
+++ Subsqueries.sql
@@ -1,6 +1,7 @@
----------- Successfull order
-
-use `new`;
+----------------------------------------------------------------------- 
+--                Successful orders
+-----------------------------------------------------------------------
+use `car store`;
 
 select `product code`,`product name`,`Quantity Ordered`, `sum of successful ordered product` from (
 with `sucess` as ( 
@@ -16,18 +17,99 @@ from
 join 
 products p using (`product code`) )s
 ;
+----------------------------------------------------------------------------------
+--                Total Product line
+----------------------------------------------------------------------------------
+select ifnull(`product line`,'Total') `Product Line`, sum(`Quantity in Stock`) `Total Quantity of stocks`, sum(price*`quantity in stock`) `Total Amount of stocks`, sum(`order value`) `Total Order Value`
+from
+(with `new` 
+as 
+(select `product line`, `Quantity in stock`, price, `order year`, `order value`
+from
+products
+join
+sales using (`product line`))
+select `Product Line`, `Quantity in stock`, price,`order value`  
+from
+`new`) s
+group by `product line` with rollup;
 
------ Number of orders in 2003
+------------------------------------------------------------------------------------
+--             Total Product line 2003
+-------------------------------------------------------------------------------------
+
+select ifnull(`product line`,'Total') `Product Line`, sum(`Quantity in Stock`) `Total Quantity of stocks`, sum(price*`quantity in stock`) `Total Amount of stocks`, sum(`order value`) `Total Order Value`
+from
+(with `new` 
+as 
+(select `product line`, `Quantity in stock`, price, `order year`, `order value`
+from
+products
+join
+sales using (`product line`)
+where `Order Year`=2003)
+select `Product Line`, `Quantity in stock`, price,`order value`  
+from
+`new`) s
+group by `product line` with rollup;
+
+------------------------------------------------------------------------------------
+--             Total Product line 2004
+-------------------------------------------------------------------------------------
+
+select ifnull(`product line`,'Total') `Product Line`, sum(`Quantity in Stock`) `Total Quantity of stocks`, sum(price*`quantity in stock`) `Total Amount of stocks`, sum(`order value`) `Total Order Value`
+from
+(with `new` 
+as 
+(select `product line`, `Quantity in stock`, price, `order year`, `order value`
+from
+products
+join
+sales using (`product line`)
+where `Order Year`=2004)
+select `Product Line`, `Quantity in stock`, price,`order value`  
+from
+`new`) s
+group by `product line` with rollup;
+
+
+------------------------------------------------------------------------------------
+--             Total Product line 2005
+-------------------------------------------------------------------------------------
+
+select ifnull(`product line`,'Total') `Product Line`, sum(`Quantity in Stock`) `Total Quantity of stocks`, sum(price*`quantity in stock`) `Total Amount of stocks`, sum(`order value`) `Total Order Value`
+from
+(with `new` 
+as 
+(select `product line`, `Quantity in stock`, price, `order year`, `order value`
+from
+products
+join
+sales using (`product line`)
+where `Order Year`=2005)
+select `Product Line`, `Quantity in stock`, price,`order value`  
+from
+`new`) s
+group by `product line` with rollup;
+
+----------------------------------------------------------------------------------  
+--     Number of orders in 2003
+----------------------------------------------------------------------------------
 SELECT count(year(`order Date`)) `Number of orders in 2003`
 FROM orders
 where year(`order Date`)= 2003;
+----------------------------------------------------------------------------------  
+--     Number of orders in 2004
+----------------------------------------------------------------------------------
 
------ Number of orders in 2004
 SELECT count(year(`order Date`)) `Number of orders in 2004`
 FROM orders
 where year(`order Date`)= 2004;
 
------ Number of orders in 2005
+----------------------------------------------------------------------------------  
+--     Number of orders in 2005
+----------------------------------------------------------------------------------
+
 SELECT count(year(`order Date`)) `Number of orders in 2005`
 FROM orders
 where year(`order Date`)= 2005;
