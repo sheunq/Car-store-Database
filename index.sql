b25a2fed0a81ae846269694ee31434fbbf536395
Seun ayegboyin <45465445+sheunq@users.noreply.github.com>
2022-04-22 16:23:05 +0100

    index

---
+++ index.sql
@@ -0,0 +1,13 @@
+create index products on
+products(`product code`, `product name`, `product line`);
+
+create index customers on
+products(`Customer name`, `contact first name`, `contact last line`);
+
+create index products on
+products(`product code`, `product name`, `product line`);
+  
+
+
+  
+  
